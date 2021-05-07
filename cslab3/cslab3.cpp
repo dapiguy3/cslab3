1ac72a1d62cfa997f0ffb4d580e1ee342ae69d30
pi <dapiguy3@gmail.com>
2021-05-07 00:00:47 -0400

    Finished

--- cslab3/cslab3.cpp
+++ cslab3/cslab3.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 using namespace std;
 
 /* part A:
@@ -44,7 +46,7 @@ void part_a() {
 	for (int i = 0; i < 5; i++) {
 		sum += third_array[i];
 	}
-	avg = sum / (sizeof(third_array)/sizeof(third_array[0]));
+	avg = sum / (sizeof(third_array) / sizeof(third_array[0]));
 	//output array,sum,average
 	cout << "array: \n";
 	for (int i = 0; i < 5; i++) {
@@ -72,9 +74,11 @@ void part_b() {
 	for (int i = 0; i < 5; i++) {
 		first_vector.push_back(i);
 	}
+	//push new value to beginning:
+	first_vector.insert(first_vector.begin(), 61);
 	//generate reverse vector:
 	vector<int> second_vector;
-	for (int i = 5; i < 0; i--) {
+	for (int i = 5; i > 0; i--) {
 		second_vector.push_back(i);
 	}
 	//output vector:
@@ -88,6 +92,90 @@ void part_b() {
 		cout << second_vector[i] << " ";
 	}
 }
+
+
+void part_c() {
+	fstream data_file; //initiate file stream
+	data_file.open("a.txt", ios::in); //open the file
+
+	string current_output; 
+	vector<int> output_vector_a;
+	int output_array_a[3][4];
+	int counter = 0;
+	//reads every line of data.txt and pushes it to output_vector
+	//i couldn't figure out how do do all of this in one loop,
+	//so it's done in two. 
+	while (getline(data_file, current_output)) {
+		output_vector_a.push_back(stoi(current_output));
+	}
+	data_file.close();
+	//slot numbers into array
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 4; x++) {
+			output_array_a[y][x] = output_vector_a[counter];
+			counter++;
+		}
+	}
+
+	//let's do it for file b
+	data_file.open("b.txt", ios::in);
+	string current_output_b = "";
+	vector<int> output_vector_b;
+	int output_array_b[3][4];
+	counter = 0;
+	while (getline(data_file, current_output_b)) {
+		output_vector_b.push_back(stoi(current_output_b));
+	}
+	data_file.close();
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 4; x++) {
+			output_array_b[y][x] = output_vector_b[counter];
+			counter++;
+		}
+	}
+
+	//compare arrays a and b to make array c
+	int output_array_c[3][4];
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 4; x++) {
+			if (output_array_a[y][x] >= output_array_b[y][x]) {
+				output_array_c[y][x] = output_array_a[y][x];
+			}
+			else {
+				output_array_c[y][x] = output_array_b[y][x];
+			}
+		}
+	}
+
+	//output the arrays
+	cout << "\noutputting array a: \n";
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 4; x++) {
+			cout << output_array_a[y][x] << "\t";
+		}
+		cout << "\n";
+	}
+	cout << "\noutputting array b: \n";
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 4; x++) {
+			cout << output_array_b[y][x] << "\t";
+		}
+		cout << "\n";
+	}
+	cout << "\noutputting array c: \n";
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 4; x++) {
+			cout << output_array_c[y][x] << "\t";
+		}
+		cout << "\n";
+	}
+
+	
+}
+
+
+
+
 int main() {
-	part_a();
+	part_c();
 }
